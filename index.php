c740e263569b8d057b88dc1f1fb54befe664e4c3
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-05-13 13:00:12 +0200

    Ejercicio 1 Nivell 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    echo '<h2>Ejercicio 1 Arrays</h2>';
+    $numeros = array("1","2","3","4","5");
+    echo 'Imprimindo valores de lo Array --> ';
+    echo $numeros[0].' ';
+    echo $numeros[1].' ';
+    echo $numeros[2].' ';
+    echo $numeros[3].' ';
+    echo $numeros[4].' ';
+?>
